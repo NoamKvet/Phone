33056e41a01758f4cd390f059a52cb6b6e55398a
Starystars67 <starystars67@gmail.com>
2018-01-17 15:31:46 +0000

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,36 @@
+resource_manifest_version 'f15e72ec-3972-4fe4-9c7d-afc5394ae207'
+
+ui_page 'html/ui.html'
+files {
+	'phone/nui.html',
+	'phone/nui.css',
+	'phone/ui.js',
+	'phone/Unkown1.png',
+	'phone/Unkown2.png',
+	'phone/Unkown3.png',
+	'phone/Unkown4.png',
+	'phone/Unkown5.png',
+	'phone/Unkown6.png',
+	'phone/Unkown7.png',
+	'phone/Unkown8.png',
+	'phone/Unkown9.png',
+	'phone/Unkown10.png',
+	'phone/Unkown11.png',
+	'phone/Unkown12.png',
+	'phone/Unkown13.png',
+	'phone/Unkown14.png',
+	'phone/Unkown15.png',
+	'phone/Unkown16.png',
+	'phone/Unkown17.png',
+	'phone/Unkown18.png',
+	'phone/Unkown19.png',
+	'phone/ContactsApp.png',
+	'phone/EmergencyServicesApp.png',
+	'phone/Phone.png',
+	'phone/PhoneBackground.png',
+	'phone/TowApp.png',
+	'phone/TwitterApp.png',
+	'phone/UberApp.png',
+}
+
+client_script "cl_phone.lua"
